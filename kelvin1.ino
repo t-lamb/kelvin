8ad1ce1bfa6959a1ee79d993c039b58ffa8a2b8d
Teresa Lamb <teresalamb@Teresas-MacBook-Pro.local>
2015-07-06 17:04:07 -0400

    removed one button, added temp sensor, add multiple temp ranges

--- kelvin1.ino
+++ kelvin1.ino
@@ -3,59 +3,74 @@
 const int button1 = 3;
 const int button2 = 2;
 const int button3 = 5;
-const int button4 = 4;
-
-//fetPin
-const int fetPin = 6;
 
 // all buttons set to LOW
 int buttonState1 = 0;
 int buttonState2 = 0;
 int buttonState3 = 0;
-int buttonState4 = 0;
+
+//fetPin
+const int fetPin = 6;
+
+//tempSensor
+const int tempPin = 0;
+int tempRead;
+float desiredTempC;
+
 
 void setup() {
   // label pins input or output
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
   pinMode(button3, INPUT);
-  pinMode(button4, INPUT);
   pinMode(fetPin, OUTPUT);
   // begin serial for readings
   Serial.begin(9600);
 }
 
-void loop() { 
+void loop() {
   //Serial.print("hello World");
-  
+
   //read state of each button
   buttonState1 = digitalRead(button1);
   buttonState2 = digitalRead(button2);
   buttonState3 = digitalRead(button3);
-  buttonState4 = digitalRead(button4);
-  
 
-  //if button pressed, say which button 
+  //read temp sensor
+  tempRead = analogRead(tempPin);
+
+  //convert reading to voltage
+  float voltage = tempRead * 5.0;
+  voltage /= 1024.0;
+  Serial.print(voltage); Serial.print(" volts");
+
+  // convert 10mv/degree with 500mv offset to degress Celsius ((voltage - 500mV) * 100)
+  float tempC = (voltage - 0.5) * 100;
+  Serial.print(tempC); Serial.println(" degrees C");
+
+  //if button pressed, say which button
   if (buttonState1 == HIGH) {
     Serial.println("pressed 1");
-    analogWrite(fetPin, 0);
+    desiredTempC = 15;
   }
   if (buttonState2 == HIGH) {
     Serial.println("pressed 2");
-    analogWrite(fetPin, 85);
+    desiredTempC = 22.5;
   }
   if (buttonState3 == HIGH) {
     Serial.println("pressed 3");
-    analogWrite(fetPin, 170);
+    desiredTempC = 30;
   }
-  if (buttonState4 == HIGH) {
-    Serial.println("pressed 4");
+
+  // check temp
+  if (tempC >= desiredTempC && tempC < desiredTempC + 2) {
     analogWrite(fetPin, 255);
   }
   else {
-    analogWrite(fetPin, 255);
+    //turn pad off
+    analogWrite(fetPin, 0);
   }
-  
-  delay(50);
+
+  delay(500);
 
 }
